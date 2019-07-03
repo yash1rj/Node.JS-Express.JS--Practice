6c0ead7921686731d8ea1bb06b2f172d04fa90d0
Yash Raj <yashrj130@gmail.com>
2019-07-03 17:10:44 +0530

    Create n3b.js

---
+++ n3-event driven arch/n3b.js
@@ -0,0 +1,12 @@
+// Here we require in the 'events' module and save a reference to it in an events variable
+let events = require('events');
+
+let listenerCallback = (data) => {
+    console.log('Celebrate ' + data);
+}
+
+let myEmitter = new events.EventEmitter();
+
+myEmitter.on('celebration', listenerCallback);
+
+myEmitter.emit('celebration', 'Life') 
