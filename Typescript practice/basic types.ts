5120664bfbbe8475c22f080bc1bc572a68f11cf7
yash1rj <yashrj130@gmail.com>
2019-08-27 00:13:35 +0530

    Create basic types.ts

---
+++ Typescript practice/basic types.ts
@@ -0,0 +1,22 @@
+
+// Declaring variables with basic types using let
+
+let productId: number = 1045; // Declaring a numeric variable
+
+let productDescription: string = '16GB, Gold'; // Declaring a string variable
+
+let productName: string = 'Samsung Galaxy J7';
+
+let productAvailable: boolean = true;   // Declaring a boolean variable
+
+console.log('The type of productId is ' + typeof productId);
+
+console.log('The type of productAvailable is ' + typeof productAvailable);
+
+console.log('The type of productName is ' + typeof productName);
+
+// Declaring variables using const
+
+const discountPercentage: number = 15;
+
+console.log('Discount percentage is:' + discountPercentage);
